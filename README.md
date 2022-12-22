3b9922ef0746a076e06301d9edce6acf89c3c011
Alper Doğukan Nizamoğlu <anizamoglu0@gmail.com>
2022-12-22 20:49:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# patika-javascript-odev-1
+ 
---
+++ clock.js
@@ -0,0 +1,4 @@
+var nickname = prompt("Lütfen Adınızı Giriniz ");
+    if (nickname != null) {
+      document.getElementById("myName").innerText = nickname;
+    }
